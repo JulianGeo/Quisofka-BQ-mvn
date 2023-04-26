e40e1c03b670748671189c0cd334161cbce0791e
Julian Rojas <julianrojasgeo@gmail.com>
2023-04-26 17:30:59 -0500

    swagger configuration in main app to allow usage in development and production servers

--- src/main/java/com/quisofka/questions/QuestionsApplication.java
+++ src/main/java/com/quisofka/questions/QuestionsApplication.java
@@ -2,11 +2,20 @@ package com.quisofka.questions;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Quisofka", version = "1.0", description = "Documentation APIs v1.0"))
+@OpenAPIDefinition(
+		info = @Info(title = "Quisofka",
+				version = "1.0",
+				description = "Documentation created for Quisofka support API in charge of providing questions"
+		),
+		servers = {
+		@Server(url = "http://localhost:8080/"),
+		@Server(url = "https://quisofka-b-production-c47e.up.railway.app/")
+})
 public class QuestionsApplication {
 
 	public static void main(String[] args) {
